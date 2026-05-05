25f41ce9755f40d59d01bde75b41b8c83f202ebd
Juan-Fdez23 <juandavidfernandezperez98@gmail.com>
2026-05-04 19:26:23 -0500

    Create UI.hpp

---
+++ Include/UI.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+double ValorAbsoluto(double x);
+double Potenciacion(double base, double exponente);
+double RaizCuadrada(double radicando);
+double Exponencial(double x);
+double Seno(double alpha);
+double Coseno(double alpha);
